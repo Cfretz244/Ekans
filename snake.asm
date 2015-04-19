81a303f30cdb55aca1883de58f8b7967b69a3782
Chris Fretz <cfretz244@gmail.com>
2015-04-19 00:20:16 -0400

    the drawing logic finally works

--- snake.asm
+++ snake.asm
@@ -1,45 +1,51 @@
 draw:
 call wipe       ; Black-out entire screen.
+xor a           ; Clear out the value in A.
+ld (D53A), a    ; Write 0 to alternating byte.
 ld hl, D540     ; Prime HL with address of board model
-ld bc, C3A0     ; Store address of current tile on screen.
+ld bc, C3A2     ; Store address of current tile on screen.
 ld d, h         ; Copy model address into de for efficiency purposes.
 ld e, l         ; ""
 ld a, 01        ; Initially seed accumulator with a 1.
 
 begin_draw:
-push af
+push af         ; Push the accumulator value so when AND over-writes it, it's not awful.
 and (hl)        ; Check if bit is set.
-jr z, draw_2nd ; Jump to next draw if not.
+jr z, draw_loop ; Jump to next draw if not.
 ld h, b         ; Get address of current tile on screen.
 ld l, c         ; ""
 ld (hl), 00     ; Load 00 byte into current tile (White tile).
 
-draw_2nd:
-pop af
+draw_loop:
 inc bc          ; Increment the current tile.
-rlca            ; Shift the accumulator for bitmasking.
-ld h, d         ; Load address of current board model byte.
-ld l, e         ; ""
-push af
-and (hl)        ; Check if bit is set.
-jr z, redraw   ; Jump to the increment logic if not.
-ld h, b         ; Get address of current tile on screen.
-ld l, c         ; ""
-ld (hl), 00     ; Load 00 byte into current tile (white tile).
-
-redraw:
-inc bc          ; Increment the current tile.
-pop af
-ld h, d
-ld l, e
+pop af          ; Restore accumulator value.
 rlca            ; Shift the accumulator for bitmasking.
 jr c, inc_model ; Shift the model forward a byte if there was a carry.
-jr begin_draw   ; Jump back to beginning if not.
+jr reload       ; Jump back to beginning if not.
 
 inc_model:
 inc de          ; Increment model forward one byte.
-ld h, d
-ld l, e
+push af         ; Store accumulator value.
+ld a, (D53A)    ; Get alternator value.
+cp a, 01        ; Check if the alternating byte is set.
+jr nz, nzero    ; Jump if it's not.
+inc bc          ; Increment the drawing location
+inc bc          ; ""
+inc bc          ; ""
+inc bc          ; ""
+dec a           ; Decrement A to zero.
+jr fix          ; Jump over set logic.
+
+nzero:
+inc a           ; Increment A to 1.
+
+fix:
+ld (D53A), a    ; Set the alteranting byte.
+pop af          ; Restore the accumulator.
+
+reload:
+ld h, d         ; Update the model.
+ld l, e         ; ""
 jr begin_draw   ; Jump back to beginning.
 
 wipe:
